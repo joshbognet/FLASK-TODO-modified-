e6fd10a3967d6b3fb9d7bc355ee105ef0959fd0f
mananuf <bmananaf@gmail.com>
2022-02-18 11:27:01 +0100

    added a delete button to the template section and a delete route with a list.remove() method.

--- todo.py
+++ todo.py
@@ -7,14 +7,27 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", todos = todo_data)
 
+
 todo_data = [] #stores data entered
+
 
 @app.route("/create-todo", methods=["POST"])
 def create_todo():
     new_todo = request.form.get("new_todo") #create a variable that stores form data
     todo_data.append(new_todo) # appends data to an array
-    print(todo_data) #prints array of things to do
+    # print(todo_data) #prints array of things to do
     return redirect(url_for("index")) #redirects back to the index page
+
+
+
+@app.route('/delete/<todo_item>')
+def delete(todo_item):
+    todo_data.remove(todo_item)
+
+    return redirect(url_for("index"))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
