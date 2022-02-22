ddc8b5a3d3072eea4779cceb545526b7c8e306aa
mananuf <bmananaf@gmail.com>
2022-02-22 11:39:19 +0100

    created update route

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+from ast import Return
 from crypt import methods
 from flask import Flask, redirect, render_template, request, url_for
 
@@ -9,6 +10,7 @@ def index():
 
 
 todo_data = [] #stores data entered
+update = []
 
 
 @app.route("/create-todo", methods=["POST"])
@@ -26,7 +28,25 @@ def delete(todo_item):
 
     return redirect(url_for("index"))
 
+item = ''
+@app.route('/update/<todo_item>', methods=['GET','POST']) #renders page to update
+def update(todo_item):
 
+    index = todo_data.index(todo_item) #gets index of item to be changed
+
+    global item     # make this variable global(accessible outside this scope)
+    item = index    
+
+    return render_template('update.html', todo_item = todo_item) # render and take todo_item
+    
+
+@app.route('/update_item', methods=['POST'])  #performs the update function
+def update_item():
+    if request.method == 'POST':
+        new_item = request.form.get('new_item')  #get that particular item u just updated from the form
+        todo_data[item] = new_item  # set the new item to index of the old item to replace the old one
+
+    return redirect(url_for('index')) # redirect to index page
 
 
 if __name__ == "__main__":
